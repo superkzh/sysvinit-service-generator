ff01fc347a07aa340056b7a374899f18dc9344c0
China Kang <997989746@qq.com>
2019-05-28 11:14:31 +0800

    Update new-service.sh

--- new-service.sh
+++ new-service.sh
@@ -6,14 +6,14 @@ if [ ! -e service.sh ]; then
   echo "--- Download template ---"
   echo "I'll now download the service.sh, because is is not downloaded."
   echo "..."
-  wget -q https://raw.githubusercontent.com/wyhasany/sample-service-script/master/service.sh
+  wget -q https://github.com/superkzh/sysvinit-service-generator/blob/master/service.sh
   if [ "$?" != 0 ]; then
     echo "I could not download the template!"
     echo "You should now download the service.sh file manualy. Run therefore:"
-    echo "wget https://raw.githubusercontent.com/wyhasany/sample-service-script/master/service.sh"
+    echo "wget https://github.com/superkzh/sysvinit-service-generator/blob/master/service.sh"
     exit 1
   else
-    echo "I donloaded the tmplate sucessfully"
+    echo "I donloaded the template sucessfully"
     echo ""
   fi
 fi
